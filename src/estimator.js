97f9ac4f6da81a434034d69c9d99634b4fda7eaa
gblessylva <gblessylva@gmail.com>
2020-04-14 14:33:22 +0100

    Estimater added

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,12 @@
 const covid19ImpactEstimator = (data) => {
-    
+    const input =data;
+    return {
+        data,
+        impact: {},
+        severeImpact:{}
+    }
+
+
 };
 
 export default covid19ImpactEstimator;
